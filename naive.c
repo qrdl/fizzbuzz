9ba38585fd21c6aab9f3bf94e1ba904d38d8455f
Ilya Caramishev <50835322+qrdl@users.noreply.github.com>
2021-01-30 12:13:00 +0000

    Update naive.c

--- naive.c
+++ naive.c
@@ -3,17 +3,17 @@
 #define LIMIT 1000000000
 
 int main(void) {
-	for (int i = 1; i <= LIMIT; i++) {
-		if (0 == i % 3) {
-			if (0 == i % 5)
-				printf("FizzBuzz\n");
-			else
-				printf("Fizz\n");
-		} else if (0 == i % 5)
-			printf("Buzz\n");
-		else
-			printf("%d\n", i);
-	}
+    for (int i = 1; i <= LIMIT; i++) {
+        if (0 == i % 3) {
+        if (0 == i % 5)
+            printf("FizzBuzz\n");
+        else
+            printf("Fizz\n");
+        } else if (0 == i % 5)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
 
-	return 0;
+    return 0;
 }
