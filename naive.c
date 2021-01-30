6887a2bd79fc04b777ba8257f33fc0a39a1b06da
Ilya Caramishev <50835322+qrdl@users.noreply.github.com>
2021-01-30 12:19:52 +0000

    Update naive.c

--- naive.c
+++ naive.c
@@ -5,14 +5,16 @@
 int main(void) {
     for (int i = 1; i <= LIMIT; i++) {
         if (0 == i % 3) {
-        if (0 == i % 5)
-            printf("FizzBuzz\n");
-        else
-            printf("Fizz\n");
-        } else if (0 == i % 5)
+            if (0 == i % 5) {
+                printf("FizzBuzz\n");
+            } else {
+                printf("Fizz\n");
+            }
+        } else if (0 == i % 5) {
             printf("Buzz\n");
-        else
+        } else {
             printf("%d\n", i);
+        }
     }
 
     return 0;
