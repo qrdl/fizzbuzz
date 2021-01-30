e81f7d4a39998542822000068de53301e17fa7e1
Ilya Caramishev <50835322+qrdl@users.noreply.github.com>
2021-01-30 12:27:03 +0000

    Update supernaive.c

--- supernaive.c
+++ supernaive.c
@@ -3,15 +3,18 @@
 #define LIMIT 1000000000
 
 int main(void) {
-	for (int i = 1; i <= LIMIT; i++) {
-		if (0 == i % 3)
+    for (int i = 1; i <= LIMIT; i++) {
+        if (0 == i % 3) {
             printf("Fizz");
-		if (0 == i % 5)
-			printf("Buzz");
-		if (i % 3 || i % 5)
-			printf("%d", i);
+        }
+        if (0 == i % 5) {
+            printf("Buzz");
+        }
+        if (i % 3 || i % 5) {
+            printf("%d", i);
+        }
         printf("\n");
-	}
+    }
 
-	return 0;
+    return 0;
 }
